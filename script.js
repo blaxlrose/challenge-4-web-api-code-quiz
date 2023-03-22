028456ecbc0da31d60480311d8a12f476b9c7d4f
christopher brown <christopherbrown@christohersMini.fios-router.home>
2023-03-22 02:15:30 -0400

    added some javascript and worked on html and css again

---
+++ script.js
@@ -0,0 +1,30 @@
+var questionsArray = [
+    {
+        question: "Commonly used data types Do Not include:",
+        imageSrc: "",
+        answerChoice: ["1.) <Strings>", "2.) <Booleans>", "3.) <Alerst>", "4.) <Numbers>"],
+        correctAnswer: 3
+    }, 
+    {
+        question: "The condition in an if/then statement is enclosed within_________.",
+        imageSrc: "",
+        answerChoice: ["1.) <Quotes>", "2.) <Curly Brackets>", "3.) <Parentheses>", "4.) <Square Brackets>"],
+        correctAnswer: 2
+    },
+    {
+        question: "Arrays in JavaScript can be used to store_________.",
+        imageSrc: "",
+        answerChoice: ["1.) <Numbers and Strings>", "2.) <Other Arrays", "3.) <Booleans>", "4.) <All of the above>"],
+        correctAnswer: 4
+    }, 
+    {
+        question: "String values must be enclosed within_______ when being assigned to variables.",
+        imageSrc: "",
+        answerChoice: ["1.) <Commas>", "2.) <Curly Brackets", "3.) <Quotes>", "4.) <Parentheses>"],
+        correctAnswer: 3
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        answerChoice: ["1.) <JavaScript>", "2.) <Terminal/Bash", "3.) <For Loops>", "4.) <Console Log>"],
+        correctAnswer: 4
+    }];
